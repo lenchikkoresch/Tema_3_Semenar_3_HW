f68eb480e72b1b46cf2a3ef701a68fde2228b3ff
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-08 00:06:53 +0300

    Семенар_3_пример_3_с_округлением

---
+++ Tema_3_Semenar_3_primer_3/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+int getCoordinate (string message)
+{int result = 0;
+while (true)
+{
+   Console.WriteLine(message);
+   if (int.TryParse(Console.ReadLine(), out result))
+   {
+    break;
+   }
+   else
+   {
+   Console.WriteLine("You've entered incorrect number");
+   }
+}
+return result;
+}
+
+double getDistance(int ax, int ay, int bx, int by)
+{
+    double result = Math.Round(Math.Sqrt(Math.Pow(ax-bx,2)+Math.Pow(ay-by,2)));
+    return result;
+}
+int ax = getCoordinate ("Enter x coordinate of the first point");
+int ay = getCoordinate ("Enter y coordinate of the first point");
+int bx = getCoordinate ("Enter x coordinate of the second point");
+int by = getCoordinate ("Enter y coordinate of the second point");
+double distance = getDistance(ax, ay, bx, by);
+Console.WriteLine($"Distance between ( {ax},{ay}) and ({bx},{by}) is {distance:f2}");
+
+
+
