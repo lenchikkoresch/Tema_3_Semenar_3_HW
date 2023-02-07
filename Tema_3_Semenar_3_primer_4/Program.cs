0f6bdaaf0b11e7ec19f604a802acb4542aa2af1d
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-08 00:31:50 +0300

    Семенар_3_пример_4

---
+++ Tema_3_Semenar_3_primer_4/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратов чисел
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите натуральное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int N = GetNumber();
+int i = 1;
+while (i < N)
+{
+    Console.Write(i * i + ",");
+    i++;
+}
+Console.Write(i * i + ".");
